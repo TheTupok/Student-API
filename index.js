db29d9eb1fa205076379ccc9432e8c9dd2c7f18b
Anatoly Tereshonok <ter.tolya@mail.ru>
2022-08-18 22:57:35 +0300

    Init base project API

---
+++ index.js
@@ -0,0 +1,14 @@
+const PORT = process.env.PORT || 5000;
+const Application = require('./framework/Application');
+const userRouter = require('./src/user-router')
+const jsonParser = require('./framework/parsejson')
+const parseUrl = require('./framework/parseUrl')
+
+const app = new Application();
+
+app.use(jsonParser);
+app.use(parseUrl('http://localhost:5000'));
+
+app.addRouter(userRouter)
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
