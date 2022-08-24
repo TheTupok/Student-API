de4c8b5b12db7d59e0e45e11fdf4ca01babb45df
Anatoly Tereshonok <ter.tolya@mail.ru>
2022-08-24 17:42:05 +0300

    file formatting

--- core/services/student-service.js
+++ core/services/student-service.js
@@ -1,28 +1,31 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class UserDatabaseService {
-    _pathDatabase = path.resolve('database/users.json')
+  _pathDatabase = path.resolve('database/users.json');
 
-    getUsersFromDatabase() {
-        return new Promise((resolve, rejects) => fs.readFile(this._pathDatabase, {encoding: 'utf-8'}, (err, data) =>{
-            if(err){
-                return rejects(err.message)
-            }
-            if(data == ''){
-                return resolve([])
-            }
-            resolve(JSON.parse(data))
-        }))
-    }
-    
-    writeUsersToDatabase(users) {
-        return new Promise((resolve, rejects) => fs.writeFile(this._pathDatabase, JSON.stringify(users, null, 4), (err) =>{
-            if(err){
-                return rejects(err.message)
-            }   
-            resolve()
-        }))
-    }
-}
+  getUsersFromDatabase() {
+    return new Promise((resolve, rejects) =>
+      fs.readFile(this._pathDatabase, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+          return rejects(err.message);
+        }
+        if (data == '') {
+          return resolve([]);
+        }
+        resolve(JSON.parse(data));
+      })
+    );
+  }
 
+  writeUsersToDatabase(users) {
+    return new Promise((resolve, rejects) =>
+      fs.writeFile(this._pathDatabase, JSON.stringify(users, null, 4), err => {
+        if (err) {
+          return rejects(err.message);
+        }
+        resolve();
+      })
+    );
+  }
+};
